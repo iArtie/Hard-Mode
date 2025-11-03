dc548b8c8134aaec79bc0d0b5f793992138fa19d
BitZ <85895666+iArtie@users.noreply.github.com>
2025-11-03 02:18:19 -0600

    Delete src/utils directory

--- src/utils/Traslate.cpp
+++
@@ -1,6 +0,0 @@
-#include <Utils/Traslate.hpp>
-
-std::string Traslate::tr(const std::string& es, const std::string& en) {
-    auto lang = Mod::get()->getSettingValue<std::string>("language");
-    return (lang == "English" ? en : es);
-}
